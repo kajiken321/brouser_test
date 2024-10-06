870eeccac2e0be597d4e2cd36b2e67a8a6aaa7e8
Kenji Kajiwara <kajiwara@chikaku.co.jp>
2024-10-06 21:03:30 +0900

    Fix ESLint errors and update components

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // You can add custom props here if needed
+  // You can add custom props here if needed in the future
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
