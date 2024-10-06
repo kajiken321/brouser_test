ac4176d2201148019aadd1218c9d090a50dea0f6
Kenji Kajiwara <kajiwara@chikaku.co.jp>
2024-10-06 21:09:26 +0900

    Fix ESLint error in input component and update files

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  // You can add custom props here if needed in the future
+  // Add a custom property to avoid the empty interface error
+  customProp?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, customProp, ...props }, ref) => {
     return (
       <input
         type={type}
